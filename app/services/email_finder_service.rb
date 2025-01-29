edde65e41bc51a6c9c220c11b755c9c5cdbb8512
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2025-01-29 21:00:32 +0000

    refactored code

--- app/services/email_finder_service.rb
+++ app/services/email_finder_service.rb
@@ -25,7 +25,16 @@ class EmailFinderService
             begin
                 puts "Processing prospect #{@processed += 1} of #{@total}: #{prospect.business_name}"   
                 content = get_website_content(prospect.website)
-
+                if content.present?
+                    email = fetch_email_from_main_page(content)
+                    if email.present?
+                        Rails.logger.info("Email found for prospect #{prospect.business_name}: #{email.join(', ')}")
+                        prospect.update(email: email.first)
+                        Rails.logger.info("Email updated for prospect #{prospect.business_name}")
+                    else
+                        Rails.logger.info("No email found for prospect #{prospect.business_name}")
+                    end
+                end
             rescue => exception
                 puts "Error processing prospect #{prospect.business_name}: #{exception.message}"
             end
@@ -35,33 +44,29 @@ class EmailFinderService
 
 
     def get_website_content(url)
-        puts "Fetching content from #{url}"
+        Rails.logger.info("Fetching content from #{url}")
         begin
             html = URI.open(url, 'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36').read
             document = Nokogiri::HTML(html)
             content = document.text
         rescue => exception
-            puts "Error processing prospect #{prospect.business_name}: #{exception.message}"
+            Rails.logger.error("Error processing prospect #{prospect.business_name}: #{exception.message}")
         end
-            
-        fetch_email_from_main_page(content) if content.present?
+        content
     end
 
     def fetch_email_from_main_page(content)
         email_pattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z]{2,}\b/i
         email = content.scan(email_pattern)
-        if email.empty?
-            Rails.logger.info("No email found in main page, fetching content from secondary pages")
-        else
+        if email.any?
             Rails.logger.info("Email found in main page #{email.join(', ')}")
-            prospect.update(email: email.first)  
+            if prospect.update(email: email.first)
+                Rails.logger.info("Email updated for prospect #{prospect.business_name}")
+            else
+                Rails.logger.error("Failed to update email for prospect #{prospect.business_name}")
+            end
         end
-        fetch_content_secondary_pages(content)
-    end
-
-
-    def fetch_content_secondary_pages(content)
-
+        email
     end
 end
 
